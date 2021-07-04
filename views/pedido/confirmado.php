1586d47138e0f580242c632a0a5bb5c28b3a1ebf
Christian Ayala <christianayala@Christians-iMac.local>
2021-07-03 22:20:50 -0500

    modification on view confirmado and edited stiles css

--- views/pedido/confirmado.php
+++ views/pedido/confirmado.php
@@ -1,76 +1,69 @@
-<?php if(isset($_SESSION['pedido']) && $_SESSION['pedido'] == 'complete'): ?>
 <section class="confirmado-pedido">
-    <div class="datos-envios">
-        <table class="datos-env">
-        <?php $id_pedido = $_SESSION['id'];?>
-        <thead>
-            <tr>
-                <th>
-                    Detalles de tu pedido:
-                </th>
-            </tr>
-        </thead>
-
-        <tbody>
-            <?php if($pedido) ?>
-                <tr>
-                    <td>Numero de pedido: <?= $id_pedido ?> <br></td>
-                </tr>
-                <tr>
-                    <td>Nombre y apellido: <?= $pedido->nombre?> <?= $pedido->apellidos ?><br></td>
-                </tr>
-                <tr>
-                    <td>Pais: <?= $pedido->pais ?> <br></td>
-                </tr>
-                <tr>
-                    <td>Departamento: <?= $pedido->departamento ?><br></td>
-                </tr>
-                <tr>
-                    <td>Ciudad: <?= $pedido->ciudad ?> <br></td>
-                </tr>
-                <tr>
-                    <td>Direccion: <?= $pedido->direccion ?> <br></td>
-                </tr>
-                <tr>
-                    <td>Total a pagar: <?= $pedido->coste ?> $ <br></td>
-                </tr>
-            <?php endif; ?>
-        </tbody>
-        </table>
-        
+<?php if(isset($_SESSION['pedido']) && $_SESSION['pedido'] == 'complete'): ?>
+    <?php $id_pedido = $_SESSION['id']; ?>
+    <div class=" mensaje-confirmado">
+        <h1>Tu pedido se ha confirmado</h1>
+        <p>
+        Tu pedido ha sido generado con éxito, una vez confirmado el pago del pedido,
+        sera procesado y enviado.
+        </p>
+        <br>
+        <h3>Datos de pago:</h1>
+        <p>Cuenta bancaria Nº 0000000000000000</p>
+    </div>
+    
+<div class="ped-confir-datos">
+<div class="datos-envios">
+        <?php if($pedido): ?>
+        <h3>Detalles de tu pedido:</h3><br>
+        <p>Nombre y apellido: <?= $pedido->nombre?> <?= $pedido->apellidos ?></p><br>
+        <p>Numero de pedido:<?= $id_pedido?> </p><br>
+        <p>Pais: <?= $pedido->pais ?></p><br>
+        <p>Departamento: <?= $pedido->departamento ?></p><br>
+        <p>Ciudad: <?= $pedido->ciudad ?></p><br>
+        <p>Direccion: <?= $pedido->direccion ?></p><br>
+        <p>Total a pagar:<?= $pedido->coste ?></p><br>
+       
     </div>
     
     <div class="tabla-confirm-producto">
-       <table class="confirmado-producto">
-        <thead>
-            <tr>
-                <th>
-                    Imagen
-                </th>
-                <th>
-                    Nombre
-                </th>
-                <th>
-                    Precio
-                </th>
-                <th>
-                    unidades
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while($producto = $productos->fetch_object()):?>
+        <h3>Productos:</h3>
+        <table class="confirmado-producto">
+            <thead>
                 <tr>
-                    <td><img src="<?= base_url ?>uploads/imgproduct/<?= $producto->imagen ?>" width="100px" alt=""></td>
-                    <td><a href="<?=base_url?>producto/ver&id_p=<?= $producto->id_p ?>"><?= $producto->nombre ?></a></td>
-                    <td><?= $producto->precio ?></td>
-                    <td><?= $producto->unidades ?></td>
+                    <th>
+                        Imagen
+                    </th>
+                    <th>
+                        Nombre
+                    </th>
+                    <th>
+                        Precio
+                    </th>
+                    <th>
+                        unidades
+                    </th>
                 </tr>
-            <?php endwhile; ?>
-        </tbody>
-       </table>
+            </thead>
+            <tbody>
+                <?php while($producto = $productos->fetch_object()):?>
+                    <tr>
+                        <td><img src="<?= base_url ?>uploads/imgproduct/<?= $producto->imagen ?>" width="100px" alt=""></td>
+                        <td><a href="<?=base_url?>producto/ver&id_p=<?= $producto->id_p ?>"><?= $producto->nombre ?></a></td>
+                        <td><?= $producto->precio ?></td>
+                        <td><?= $producto->unidades ?></td>
+                    </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
     </div>
+
+        
+  
+    <?php endif ?>
+</div>
+    
+<?php elseif(isset($_SESSION['pedido']) && $_SESSION['pedido'] != 'complete'): ?>
+    <p>Tu pedido no ha podido procesarse</p>
+<?php endif; ?>
 </section>
-
-
-

7dc9f4498054beb6011c34eda08565600d473034
Christian Ayala <christianayala@Christians-iMac.local>
2021-07-08 18:37:44 -0500

    adding function to send email to support in contaccontroller and modification in view contacto

--- controllers/contactoController.php
+++ controllers/contactoController.php
@@ -8,4 +8,27 @@ class contactoController{
         require_once 'views/contacto/contacto.php';
     }
 
+    public function send(){
+        $nombre = $_POST['nombre'];
+        $telefono = $_POST['telefono'];
+        $mail = $_POST['email'];
+
+        $header = 'From: ' . $mail . " \r\n";
+        $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+        $header .= "Mime-Version: 1.0 \r\n";
+        $header .= "Content-Type: text/plain";
+
+        $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+        $mensaje .= "Su e-mail es: " . $mail . " \r\n";
+        $mensaje .= "Su Telefono es:" . $telefono . "\r\n";
+        $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+        $mensaje .= "Enviado el " . date('d/m/Y', time());
+
+        $para = 'crivlaayacon@gmail.com';
+        $asunto = 'Mensaje de mi sitio web';
+
+        mail($para, $asunto, utf8_decode($mensaje), $header);
+
+        header("Location:". base_url. 'contacto/index');
+    }
 }
--- views/contacto/contacto.php
+++ views/contacto/contacto.php
@@ -9,21 +9,21 @@
                 <i class="fas fa-map-marker-alt"></i> Av. 3 #15-35 Barrio Primavera
             </p> <br> <br> <br> 
             <div id="mapid" class="map">
-            </div> <br> <br> <br> 
-            <i class="fab fa-instagram"> <a href="https://www.instagram.com/mobacoffeecompany/" class="follow-instagram">Síguenos en Instagram</a> </i>  <br> <br> <br> 
+            </div> <br> <br>
+            <i class="fab fa-instagram"></i>  <a href="https://www.instagram.com/mobacoffeecompany/" class="follow-instagram">Síguenos en Instagram</a> <br> <br> 
         </div>
     </div>
     <div class="formulario-contac">
-        <form action="#" method="POST" class="form-contactanos">
+        <form action="<?= base_url ?>contacto/send" method="POST" class="form-contactanos">
             <h1>ENVIANOS UN MENSAJE</h1> <br> <br>
             <label for="nombre">Nombre</label> <br>
-            <input type="text"> <br>
+            <input type="text" name="nombre"> <br>
             <label for="nombre">Telefono</label> <br>
-            <input type="number"> <br>
+            <input type="number" name="telefono"> <br>
             <label for="ciudad">Ciudad</label><br>
-            <input type="text"> <br>
+            <input type="text" name="ciudad"> <br>
             <label for="email">E-mail</label><br>
-            <input type="text"> <br>
+            <input type="text" name="email"> <br>
             <label for="mensaje">Mensaje</label> <br>
             <textarea name="mensaje" id="" cols="30" rows="10"></textarea> <br>
             <input type="submit" value="Enviar" class="boton-form"> <br>
